30591601984031a06060710094ae4b38d1884051
giorgio <giorgio.rinchi@gmail.com>
2020-02-21 16:48:13 +0100

    aggiunte due label

--- src/AwayFromTheMilkyWay/view/PlayerDataPane.java
+++ src/AwayFromTheMilkyWay/view/PlayerDataPane.java
@@ -21,23 +21,27 @@ public class PlayerDataPane extends GridPane {
     public void populatePane(){
     
         Label rimbalziDesiderati = new Label("Rimbalzi Desiderati: ");
-        rimbalziDesiderati.setPrefSize(600, 90);
+        rimbalziDesiderati.setPrefSize(400, 60);
         Label nome  = new Label("nome: ");
-        nome.setPrefSize(600, 90);
+        nome.setPrefSize(400, 60);
         Label forza = new Label("forza: ");
-        forza.setPrefSize(600, 90);
+        forza.setPrefSize(400, 60);
         Label rimbalziEffettuati = new Label("rimbalzi effettuati: ");
-        rimbalziEffettuati.setPrefSize(600, 90);
+        rimbalziEffettuati.setPrefSize(400, 60);
+        Label numLivello = new Label("livello numero ");
+        numLivello.setPrefSize(400, 60);
+        Label opzioni = new Label("opzioni");
+        opzioni.setPrefSize(400, 60);
+        
         
         
         this.add(rimbalziDesiderati,0,0);
         this.add(nome,1,0);
         this.add(forza,0,1);
         this.add(rimbalziEffettuati,1,1);
-        /*this.add(new Label("a"),0,0);
-        this.add(new Label("a"),1,0);
-        this.add(new Label("a"),0,1);
-        this.add(new Label("a"),1,1);*/
+        this.add(numLivello,2,0);
+        this.add(opzioni,2,1);
+        
     }
     
 }
